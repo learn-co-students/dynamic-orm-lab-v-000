efe777cc3d962dd37450fb830afa2ba8eff048e5
febbraiod <donedesign1@gmail.com>
2016-01-21 11:52:55 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,13 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
+
+
+  self.column_names.each do |att|
+    attr_accessor att.to_sym
+  end
 
 end
