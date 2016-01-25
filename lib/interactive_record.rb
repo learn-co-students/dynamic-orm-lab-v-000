b44b47f44e8143a0b6d4e4e5def897ad91d65f57
azheng249 <andy.zheng249@gmail.com>
2016-01-25 16:19:46 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,13 +4,13 @@ require 'active_support/inflector'
 class InteractiveRecord
   
   def self.table_name
-    self.class.to_s.pluralize
+    self.to_s.downcase.pluralize
   end
 
   def self.column_names
     DB[:conn].results_as_hash = true
 
-    sql = "pragma table_info(#{table_name}"
+    sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
@@ -25,26 +25,42 @@ class InteractiveRecord
   end
 
   def save
-    sql = "INSERT INTO #{self.class.table_name} (#{column_names_for_insert}) VALUES (#{values_for_insert});"
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert});"
 
     DB[:conn].execute(sql)
-    @id = "SELECT MAX(id) FROM #{self.class.table_name};"
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.class.table_name};")[0][0]
   end
 
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name| 
-      values << "#{send(col_name)}" unless send(col_name).nil?
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
   end
 
-  def column_names_for_insert
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
-  def self.find_by_name
-    sql = "SELECT * FROM #{self.class.table_name} WHERE name = '#{name}';"
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}';"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(attributes)
+    student_query = []
+    attributes.each do |attr, value|
+      value = "'#{value}'" if value.class == String
+      student_query << attr.to_s + " = #{value}"
+    end
+    student_query = student_query.join(" AND ")
+
+    sql = "SELECT * FROM #{self.table_name} WHERE #{student_query};"
     DB[:conn].execute(sql)
   end
 
