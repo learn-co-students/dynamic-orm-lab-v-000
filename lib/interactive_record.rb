791f75a8197f3bb3685bb3d889d6369c241b8f0f
shmulim <shmuel.markel@gmail.com>
2016-02-02 16:40:27 -0800

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,22 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+require 'pry'
+
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    col_array = []
+    sql = <<-SQL
+      PRAGMA table_info('students')
+    SQL
+    DB[:conn].execute(sql).each { |idx| col_array << idx['name']}
+    col_array
+  end
 end
+
+#binding.pry
