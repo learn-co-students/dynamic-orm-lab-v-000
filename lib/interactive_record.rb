bbb8963fca49fcff24196e47b047456361f5a4ae
shmulim <shmuel.markel@gmail.com>
2016-02-07 21:14:57 -0800

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,6 +5,12 @@ require 'pry'
 
 class InteractiveRecord
 
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -17,6 +23,18 @@ class InteractiveRecord
     DB[:conn].execute(sql).each { |idx| col_array << idx['name']}
     col_array
   end
+
+  def col_names_for_insert
+    self.class.column_names.drop(1).join(', ')
+  end
+
+  def values_for_insert
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
 end
 
-#binding.pry
+# binding.pry
