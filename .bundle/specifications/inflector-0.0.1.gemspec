7616078b50e7081a25bfe881bf68c6c4b35d813c
Chris Haugen <haugenc@gmail.com>
2016-03-28 17:09:08 +0000

    Done.

---
+++ .bundle/specifications/inflector-0.0.1.gemspec
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+# stub: inflector 0.0.1 ruby lib
+
+Gem::Specification.new do |s|
+  s.name = "inflector"
+  s.version = "0.0.1"
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.require_paths = ["lib"]
+  s.authors = ["Thomas Monzel"]
+  s.date = "2012-10-22"
+  s.description = "A simple class for plural-, singular-, camel-, pascal-, constantization and underscore"
+  s.email = "tom.monzel@gmail.com"
+  s.homepage = "https://github.com/tmonzel/inflector"
+  s.rubygems_version = "2.4.3"
+  s.summary = "English inflections"
+
+  s.installed_by_version = "2.4.3" if s.respond_to? :installed_by_version
+end
--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,72 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
+
+  #///CLASS METHODS///#
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = "pragma table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    column_names = Array.new
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+
+  #self.column_names.each do |col_name|
+    #binding.pry
+    #attr_accessor col_name.to_sym
+  #end
+
+  def initialize (args={})
+    args.each do |property, value|
+      #binding.pry
+      self.send("#{property}=",value)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
   
+  def self.find_by(args)
+    sql = "SELECT * FROM #{table_name} WHERE #{args.keys[0]} = '#{args.values[0]}'"
+    DB[:conn].execute(sql)
+  end
+
+  #///INSTANCE METHODS///#
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    cols = []
+    self.class.column_names.each do |col_name|
+      cols << col_name  unless col_name == 'id'
+    end
+    cols.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
 end
