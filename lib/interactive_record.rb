688f99eae3706bf0211d785072de83fe88fd94d2
Richard-Frost <frost.rchrd@gmail.com>
2016-04-01 16:47:24 -0700

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,16 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+
 end
+
+def update
+    sql = <<-SQL
+    UPDATE students 
+    SET name = ?, 
+    grade = ? 
+    WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
