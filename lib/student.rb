c6a2e652642080396ad477d7ac0c0369c2a0d038
Richard-Frost <frost.rchrd@gmail.com>
2016-04-03 08:42:37 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -69,14 +69,13 @@ class Student < InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
-  
   def self.find_by(attribute_hash)
-    value = attribute_hash.values.first
-    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    value = attribute_hash.values.first #sets a variable = to the value of the hash
+    formatted_value = value.class == Fixnum ? value : "'#{value}'" 
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
     DB[:conn].execute(sql)
+   
   end
-  
 end
 
 
