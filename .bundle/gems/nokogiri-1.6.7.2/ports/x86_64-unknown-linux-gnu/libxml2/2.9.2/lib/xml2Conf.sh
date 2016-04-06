6953375ab9e3db752f554f068b7ab58451db6824
hide83 <hiral.desai83@gmail.com>
2016-04-06 15:04:54 +0000

    Done.

---
+++ .bundle/gems/nokogiri-1.6.7.2/ports/x86_64-unknown-linux-gnu/libxml2/2.9.2/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/nitrous/code/labs/dynamic-orm-lab-v-000/.bundle/gems/nokogiri-1.6.7.2/ports/x86_64-unknown-linux-gnu/libxml2/2.9.2/lib"
+XML2_LIBS="-lxml2 -lz   -lm "
+XML2_INCLUDEDIR="-I/home/nitrous/code/labs/dynamic-orm-lab-v-000/.bundle/gems/nokogiri-1.6.7.2/ports/x86_64-unknown-linux-gnu/libxml2/2.9.2/include/libxml2"
+MODULE_VERSION="xml2-2.9.2"
+
