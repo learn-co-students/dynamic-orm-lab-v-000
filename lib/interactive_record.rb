618ef63db9fa8967ccfc0a57b586c5835dcd0120
katherinethai <kthai1618@gmail.com>
2016-04-13 16:22:53 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,7 +48,7 @@ class InteractiveRecord
 
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-    DB[:conn].execute(sql,name)
+    DB[:conn].execute(sql)
   end
 
   def self.find_by(attribute_hash)
