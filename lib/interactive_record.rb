c5e16dcb0adee7b83e2bffaa5283b18c23bdd61c
hangloos <ericloos00@gmail.com>
2016-06-01 16:37:16 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,24 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
+
+
   
 end
+
+
+
+
+
+
+
+
+
+
+
+#get table name
+#open up the details of the table to get the columns
+#use those columns to create attributes
+# 
