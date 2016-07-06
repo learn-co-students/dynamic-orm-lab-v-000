db8f5c8563a0fbf9e723f530ac5608c638f28cd5
tpstar <hanlee12@gmail.com>
2016-07-05 22:50:18 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,14 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+
+  def self.find_by(name: nil, grade: nil)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ? OR grade = ?"
+    DB[:conn].execute(sql, name, grade)
+  end
+
 end
