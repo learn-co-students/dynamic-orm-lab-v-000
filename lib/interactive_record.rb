2427a4b990e794e4d639190b7db256134987094c
Lenn Hypolite <lenntriniman@hotmail.com>
2016-08-16 22:11:28 +0000

    Done

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,7 +7,7 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
-  # get column names
+  # --get column names
   def self.column_names
     DB[:conn].results_as_hash = true
     sql = "PRAGMA table_info('#{table_name}')"
@@ -20,24 +20,60 @@ class InteractiveRecord
     end
 
     column_names.compact
-    # binding.pry
-
   end
 
-  # create attr_accessors for each column name
+  # --create attr_accessors for each column name
   self.column_names.each do |col_name|
    attr_accessor col_name.to_sym
   end
 
   def initialize(options={})
+    # --invoking a method via the #send method
     options.each do |property, value|
       self.send("#{property}=", value)
+    end
   end
 
+  # --return the table name when called on an instance of Student
+  # --get the table names to use as an abstract in the save method
+  def table_name_for_insert
+    self.class.table_name
   end
 
-  # def self.find_by
-  #
-  # end
+  # --return the column names when called on an instance of Student
+  # --get the table names to use as an abstract in the save method
+  def col_names_for_insert
+    self.class.column_names.delete_if { |col| col == "id" }.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by(hash)
+    key_of = nil
+    value = nil
+    hash.each do |key,val|
+      key_of = key
+      value = val
+    end
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key_of} = '#{value}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 
 end
