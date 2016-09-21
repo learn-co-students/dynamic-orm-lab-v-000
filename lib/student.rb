dff0ca4fb9163ab329b7600049be6dc58935c106
David Kennell <davidkennell0@gmail.com>
2016-09-21 03:41:47 +0000

    Finished everything  excepts #find_by

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,16 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+  
+  def initialize(options = {})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+  
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+  
+  
 end
