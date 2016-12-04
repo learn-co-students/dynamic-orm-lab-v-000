57248b8ca80995c217440a93d457e59ee19a6be2
Pia Kim <pkim2320@gmail.com>
2016-12-04 04:58:45 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,15 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+
+    # We can tell our Song class that it should have an attr_accessor named after each column name with the following code:
+
+      # This is metaprogramming because we are writing code that writes code for us.
+      # By setting the attr_accessors in this way, a reader and writer method for each column name
+      # is dynamically created, without us ever having to explicitly name each of these methods.
+
+
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 end
