9baa8328fef099b21e5d2881c4fa208ec5b8b0d0
Juan Gongora <gongora.animations@gmail.com>
2017-03-07 04:26:25 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
@@ -9,7 +10,7 @@ class InteractiveRecord
 
   def self.column_names
     DB[:conn].results_as_hash = true
-    sql = "PRAGMA table_info(#{self.table_name})"
+    sql = "PRAGMA table_info('#{self.table_name}')"
 
     columns_query = DB[:conn].execute(sql)
     columns_compiler = []
@@ -45,24 +46,26 @@ class InteractiveRecord
     value.join(", ")
   end
 
-    def save
-      sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
 
-      DB[:conn].execute(sql)
+    DB[:conn].execute(sql)
 
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name=(?)"
+
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(hash_value)
+    sql = nil
+    hash_value.each do |key, value|
+      sql = "SELECT * FROM #{self.table_name} WHERE #{key.to_s} = '#{value.to_s}'"
     end
-
-    def self.find_by_name(name)
-      sql = "SELECT * FROM #{self.table_name} WHERE name=(?)"
-
-      DB[:conn].execute(sql, name)
-    end
-
-    def self.find_by(value)
-      sql = "SELECT * FROM #{self.table_name} WHERE ?=(?)"
-
-      DB[:conn].execute(sql, value)
-    end
+    DB[:conn].execute(sql)
+  end
 
 end
