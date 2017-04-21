e17785d2143a4155087154d2b74ea0f212b49005
ETphonehomeslice <mk.etlinger@gmail.com>
2017-04-21 10:29:00 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,10 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require 'interactive_record.rb'
+require 'interactive_record'
 
 class Student < InteractiveRecord
+
+  self.column_names.each {|col_name| attr_accessor col_name.to_sym}
+
 
 end
