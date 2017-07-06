87db8b8e01623ebd99d12208deaa88baefa31423
emhopkins <epoletto@lstreetc.com>
2017-07-05 23:57:05 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,23 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
+
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+  def self.find_by(hash)
+  	k = ""
+  	v = ""
+  	hash.each do |key, value|
+  		k = key.to_s
+  		v = value
+  	end
+  	sql = "SELECT * FROM #{self.table_name} WHERE #{k} = '#{v}'"
+   	DB[:conn].execute(sql)
+  end
 
 end
