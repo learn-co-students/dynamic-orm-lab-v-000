5baa0fb8ba50bd6c8358f3221d97846b80f95f03
Elvin <Dimix181@Gmail.com>
2017-08-30 12:04:34 -0400

    adding attributes to instance methods

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,5 +4,18 @@ require 'pry'
 
 class InteractiveRecord
 
-  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    sql = "pragma table_info('#{table_name}')"
+    DB[:conn].execute(sql).collect do |row|
+      row["name"]
+    end
+  end
+
+  def initialize(attributes={})
+    attributes.each{|k,v| self.send("#{k}=",v) unless k ="id"}
+  end
 end
