a56623ada491b6eb5709a4c614dc3b5f959ab86d
Mariel F <marielbeth@gmail.com>
2017-08-31 19:35:23 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,70 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    table_info.map do |column|
+      column["name"]
+    end.compact
+  end
+
+  def initialize(attr_hash = {})
+    attr_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col_name| col_name == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    self.class.column_names.map do |col_name|
+      "'#{send(col_name)}'" unless send(col_name).nil?
+    end.compact.join(", ")
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO  #{table_name_for_insert} (#{col_names_for_insert})
+      VALUES (#{values_for_insert})
+    SQL
+
+    DB[:conn].execute(sql)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM #{self.table_name}
+      WHERE name = '#{name}'
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(kv_pair)
+    key = kv_pair.keys[0]
+    value = kv_pair[key]
+
+    sql = <<-SQL
+      SELECT *
+      FROM #{self.table_name}
+      WHERE #{key} = '#{value}'
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 end
