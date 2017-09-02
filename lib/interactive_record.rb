c2e73cccfaefa3ee616036bb6e5ce3262fa57903
Elvin <Dimix181@Gmail.com>
2017-09-01 20:41:42 -0400

    add test are passing

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,10 +11,23 @@ class InteractiveRecord
 
   def self.column_names
     sql = "pragma table_info('#{table_name}')"
-    DB[:conn].execute(sql).collect do |row|
-      row["name"]
-    end
+    DB[:conn].execute(sql).collect{ |row| row["name"] }
     #returns an array of all the columns name
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM #{table_name} WHERE name = ?
+    SQL
+    DB[:conn].execute(sql,name)
+  end
+
+  def self.find_by(attribute={})
+    DB[:conn].results_as_hash = true
+    sql = <<-SQL
+      SELECT * FROM #{table_name} WHERE name = ? OR grade = ?
+    SQL
+    DB[:conn].execute(sql,attribute.values.first, attribute.values.first)
   end
 
   def initialize(attributes={})
@@ -26,8 +39,24 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    #binding.pry
-    self.class.column_names
+    #same as class.column_names w/o id
+    #returns stings
+    self.class.column_names.reject{|i| i=="id"}.join(", ")
+  end
+
+  def values_for_insert
+      result = col_names_for_insert.split(", ").collect do |col|
+         "'#{send(col)}'"
+      end
+      result.join(", ")
+  end
+
+  def save
+      sql = <<-SQL
+        INSERT INTO #{self.class.table_name} (name, grade) VALUES(?,?)
+      SQL
+      DB[:conn].execute(sql,self.name,self.grade)
+      @id = DB[:conn].last_insert_row_id
   end
 
 end
