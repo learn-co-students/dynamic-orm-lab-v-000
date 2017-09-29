20a155e96507db8b046e9143452f208178735ed4
Johan Esteves <jsesteves91@gmail.com>
2017-09-29 01:49:20 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,7 +60,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    #binding.pry
+    binding.pry
     sql = <<-SQL
     SELECT * FROM #{self.table_name}
     WHERE "#{attribute.keys.join("")}" = ?
