9465e7f4f293a8194f64fdb34508be000d663c26
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-10-21 18:42:02 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,8 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  self.column_names.each do |col_name|      #now our class only needs one method to set attr_accessors specific
+    attr_accessor col_name.to_sym           #to this class, everything else is taken care of by :InteractiveRecord
+  end
+
 end
