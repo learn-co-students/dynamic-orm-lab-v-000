974af79f2f5070d5b771e9ae4ebb2205120ca24f
Denis Bondin <dsbondin@gmail.com>
2017-10-24 01:19:31 +0000

    initialize

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,24 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    sql = "PRAGMA table_info(#{self.table_name})"
+    col_names = []
+    DB[:conn].execute(sql).each do |col|
+      col_names << col["name"]
+    end
+    col_names
+  end
+
+  def initialize(attributes = {})
+    attributes.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+  end
+
 end
