8da1f5b9cb6e3251ee3b1b046ae8cbf74eba82bd
Denis Bondin <dsbondin@gmail.com>
2017-10-24 02:44:18 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,4 +38,36 @@ class InteractiveRecord
     values.join(", ")
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO #{table_name_for_insert}
+      (#{col_names_for_insert})
+      VALUES (#{values_for_insert});
+    SQL
+
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert};")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name}
+      WHERE name = ?;
+    SQL
+
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(hash)
+    key = hash.keys[0]
+    value = hash.values[0]
+
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name}
+      WHERE #{key} = ?;
+    SQL
+
+    DB[:conn].execute(sql, value)
+  end
+
 end
