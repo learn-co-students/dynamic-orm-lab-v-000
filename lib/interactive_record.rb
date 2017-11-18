e090c1b2d41b5c97769692c2ddf285d08f1618eb
Susan Fowler <infopro94@gmail.com>
2017-11-18 03:08:21 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
@@ -17,19 +18,36 @@ class InteractiveRecord
     table_info.each do |column|
       column_names << column["name"]
     end
-
       column_names.compact
   end
 
-    self.column_names.each do |col_name|
-      attr_accessor col_name.to_sym
-  end
+  #   self.column_names.each do |col_name|
+  #     attr_accessor col_name.to_sym
+  # end
 
   def initialize(options={})
 	    options.each do |property, value|
         self.send("#{property}=", value)
+        binding.pry
       end
+      Student.new
   end
 
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  # self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  # end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
 
 end
