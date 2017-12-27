e84468c3b8eb1ff21262efebadc1a6fe332bd2b7
github username <github email address>
2017-12-27 13:07:03 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,23 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+    attr_accessor :name, :album
+    attr_reader :id
+ 
+    def initialize(id=nil, name, album)
+        @id = id
+        @name = name
+        @album = album
+    end
+
+    def self.table_name
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            grade INTEGER
+        ) 
+        SQL 
+        DB[:conn].execute(sql)
+    end
 end
