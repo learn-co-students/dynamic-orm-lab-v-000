7855e0becdc44dc77d224c49db73bd85db4e154a
github username <github email address>
2017-12-27 13:37:58 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,15 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+require 'pry'
+
 class InteractiveRecord
   
+    def self.table_name
+        Student.inspect.downcase + "s"
+        # binding.pry
+    end
+
+    def column_name
+    end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,26 +1,8 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
 
-    attr_accessor :name, :album
-    attr_reader :id
- 
-    def initialize(id=nil, name, album)
-        @id = id
-        @name = name
-        @album = album
-    end
-
-    def self.table_name
-        sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS students (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            grade INTEGER
-        ) 
-        SQL 
-        DB[:conn].execute(sql)
-    end
 end
