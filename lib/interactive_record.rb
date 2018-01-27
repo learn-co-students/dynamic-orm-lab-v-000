fe7bb383142a173acd0eb36add5d84fb1c8d5886
James Ferguson <ncjamesferguson@gmail.com>
2018-01-27 22:41:45 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -47,5 +47,9 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+  def self.find_by_name
+    sql = nil
+  end
+
 
 end
