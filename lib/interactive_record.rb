45c285824d426af9bcfff012f00798a0f220e7c8
Blakeley Bond <burrick2003@yahoo.com>
2018-01-29 03:06:07 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,9 +53,11 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-
-
-
-
+  def self.find_by(input) #input is a hasheshes
+    first_hash = input.values.first
+    input_value = first_hash.class == Fixnum ? first_hash : "'#{first_hash}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{input.keys.first} = #{input_value}"
+    DB[:conn].execute(sql)
+  end
 
 end
