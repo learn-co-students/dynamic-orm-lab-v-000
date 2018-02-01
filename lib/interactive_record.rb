e0278027f4291b91bbc13115802cc72e825ef56a
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-02-01 03:59:28 -0500

    ALL TESTS PASSING

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'pry'
 
+#done
 class InteractiveRecord
     def self.table_name
         self.name.to_s.downcase.pluralize
