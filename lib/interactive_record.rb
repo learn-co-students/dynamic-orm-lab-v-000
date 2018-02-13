d554e4ffe24741d12525d7ecea14e6a48b6849ab
smithwebtek <brad@smithwebtek.com>
2018-02-13 14:36:40 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,5 +55,6 @@ class InteractiveRecord
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
     DB[:conn].execute(sql, name)
-  end
+	end
+	binding.pry
 end
