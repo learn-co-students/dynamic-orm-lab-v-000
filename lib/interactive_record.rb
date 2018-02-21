d026e1abab48ffaa4aece168db5a7a6a7feef915
johnwojo <jwojo13@gmail.com>
2018-02-21 12:30:08 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,62 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+    def initialize(options={})
+      options.each do |property, value|
+        self.send("#{property}=",value)
+      end
+    end
+
+    def self.table_name
+      "#{self.to_s.downcase}" + "s"
+    end
+
+    def self.column_names
+      DB[:conn].results_as_hash = true
+
+      sql = "pragma table_info('#{table_name}')"
+
+      table_info = DB[:conn].execute(sql)
+      columns = []
+      table_info.each do |row|
+        columns << row["name"]
+      end
+      columns
+    end
+
+
+    def table_name_for_insert
+      self.class.table_name
+    end
+
+    def col_names_for_insert
+      self.class.column_names.delete_if {|column| column == "id"}.join(", ")
+    end
+
+    def values_for_insert
+      values_array = []
+      self.class.column_names.each do |name|
+        values_array << "'#{send(name)}'" unless send(name).nil?
+      end
+      values_array.join(", ")
+    end
+
+    def save
+      sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+      DB[:conn].execute(sql)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+
+    def self.find_by_name(name)
+      sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+      DB[:conn].execute(sql)
+    end
+
+    def self.find_by(attributes)
+      value = attributes.values.first
+      formatted_value = value.class == Fixnum ? value : "'#{value}'"
+      sql = "SELECT * FROM #{self.table_name} WHERE #{attributes.keys.first} = #{formatted_value}"
+      DB[:conn].execute(sql)
+    end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  def self.table_name
-    "#{self.to_s.downcase}" + "s"
+  self.column_names.each do |column|
+    attr_accessor column.to_sym
   end
 end
