dd2293d51cf85161eddb04c0f3cac9aac9b27bcf
Tyler Pulse <tyler3321@gmail.com>
2018-02-22 17:07:55 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,10 +53,16 @@ class InteractiveRecord
     end
     
     def save
-       sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-            DB[:conn].execute(sql)
-       @id = DB[:conn].execute("SELECT last_insert_rowid() from #{table_name_for_insert}")[0][0]
+        sql = <<-SQL
+            INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+            VALUES (#{values_for_insert})
+        SQL
+        
+        DB[:conn].execute(sql)
+        
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
     end
+
     
    def self.find_by_name(name)
         sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
