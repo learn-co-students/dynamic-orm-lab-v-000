0e24067d68421ffd5ed0deec2f03bd30ddcbfd44
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-11 03:43:11 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,22 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.pluralize.downcase
+  end
+
+  def self.column_names
+    sql = "PRAGMA table_info( #{self.table_name} )"
+    columns_info = DB[:conn].execute(sql)
+    columns_name = []
+    columns_info.each do |column|
+      columns_name << column[1]
+    end
+    columns_name
+  end
+
+
 end
