c45b90c1a362ae42c274a0688b1a980ed81bc8e2
Aveline <amouraveline@gmail.com>
2018-03-19 17:51:34 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,14 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+  def self.all
+    sql = "SELECT * FROM #{self.table_name}"
+    DB[:conn].execute(sql)
+  end
+
 
 end
