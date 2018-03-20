d63ed4056790404507d045edf92c876cf426cd88
Sam. O <samrey2018@gmail.com>
2018-03-20 13:40:38 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,68 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+        def self.table_name
+          self.to_s.downcase.pluralize
+        end
+
+        def self.column_names
+          DB[:conn].results_as_hash = true
+
+          sql = "PRAGMA table_info('#{table_name}')"
+
+          table_info = DB[:conn].execute(sql)
+          column_names = [ ]
+
+          table_info.each do |column|
+            column_names << column["name"]
+          end
+          column_names.compact
+        end
+
+                self.column_names.each do |col_name|
+          attr_accessor col_name.to_sym
+        end
+
+        def initialize(options={})
+                options.each do |property, value|
+            self.send("#{property}=", value)
+          end
+        end
+
+        def table_name_for_insert
+                self.class.some_class_method
+        end
+
+        def col_names_for_insert
+          #abstract the column_names
+                self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+        end
+
+        def values_for_insert
+                values = [ ]
+                self.class.column_names.each do |col_name|
+                        values << "'#{send(col_name)}'" unless send(col_name).nil?
+          end
+                values.join(", ")
+        end
+
+        def save
+        #   uses the last three ceated methods.
+                sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+
+                DB[:conn].execute(sql)
+
+                @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+        end
+
+        def self.find_by_name(name)
+                sql = "SELECT * FROM #{self.table_name} WHERE name = #{name}"
+                DB[:conn].execute(sql)
+        end
+
+        def find_by
+                sql = "SELECT * FROM #{self.table_name} WHERE ? = #{name}"
+                DB[:conn].execute(sql)
+
+        end
 end
