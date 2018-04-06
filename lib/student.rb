c1a03fd5f09e57637e9a5b7ab81d041ca2c00d01
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-06 22:20:03 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,16 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+
+  self.column_names.each do |column|
+    attr_accessor column.to_sym
+
+  end
+
+  def initialize(hash = {})
+    hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
 end
