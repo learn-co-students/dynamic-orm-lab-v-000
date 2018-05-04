a44e33d0f414d79d528e5a9c54961da7daf83ce9
osxgirl <osxgirl@mac.com>
2018-05-03 22:51:48 -0400

    //❤️

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,10 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
+
 class Student < InteractiveRecord
+    self.column_names.each do |col|
+    attr_accessor col.to_sym
+end
 
 end
