0d3ce3f230af016874c3eff5aa1cf8ef316251f6
sgibson47 <sgibson47@gmail.com>
2018-06-15 18:21:23 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
@@ -32,5 +33,47 @@ class InteractiveRecord
   def table_name_for_insert
     self.class.table_name
   end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col|
+      col == "id"}.join(", ")
+    # self.class.column_names returns an array of column names
+    # .delete_if {|col| col == "id"} removes the id column b/c when we insert a row we don't add an id attr
+    # .join(", ") converts or array to a string and places ", " between the array elements
+    # .join(", ") converts the arrary into the string we want for our instert stmnt
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+    # iterate over result of #column_names & use #send to call reader methods on an instance and grab attr values to insert
+    # so many quotes! "" b/c ultimately we're trying to craft a string of SQL
+    # each value gets '' b/c our SQL stmnt ought to look like VALUES 'value1', 'value2' 
+    # values is an array, so we use .join to combine the elements into one string
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert}(#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    # put all the for_insert methods to work to insert a row
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    # grab the newly set id value
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+    # select the row from this table where name matches argument
+  end
+
+  def self.find_by(**keyword_arg)
+    arr = keyword_arg.to_a
+    sql = "SELECT * FROM #{self.table_name} WHERE #{arr[0][0].to_s} = '#{arr[0][1]}'"
+    DB[:conn].execute(sql)
+  end
+
   
 end
