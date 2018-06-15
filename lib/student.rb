3525995a5dbbc7e7730c1eda74928e901a3aa482
sgibson47 <sgibson47@gmail.com>
2018-06-15 17:12:30 -0500

    add attr_accessors via metaprogramming

--- lib/student.rb
+++ lib/student.rb
@@ -4,5 +4,10 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
   
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+  # iterate over array of column names andwrite an attr_accessor for each
+  # .to_sym turns string into a symbol
 
 end
