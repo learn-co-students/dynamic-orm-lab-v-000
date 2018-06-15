3b9055544d63320e8291d0684b263f66c6c9f026
sgibson47 <sgibson47@gmail.com>
2018-06-15 17:21:53 -0500

    add #initialize

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,21 @@ class Student < InteractiveRecord
   
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
+    # iterate over array of column names andwrite an attr_accessor for each
+    # .to_sym turns string into a symbol
   end
-  # iterate over array of column names andwrite an attr_accessor for each
-  # .to_sym turns string into a symbol
+  
+
+  def initialize(options={})
+    options.each do |prop, value|
+      self.send("#{prop}=", value)
+    end
+    # iterate over hash and 
+    # use #send to call a method whose name is the interpolated prop/key equals
+    # and to pass the value in as an argument of prop/key=
+    # this sets an instance's attributes based on key & value pairs
+  end
+
+  
 
 end
