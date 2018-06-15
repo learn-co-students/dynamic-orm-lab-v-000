217f11e90ca2fff709f718c866fcfaf9eaf3ad60
sgibson47 <sgibson47@gmail.com>
2018-06-15 17:07:22 -0500

    add #column_names

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -6,5 +6,27 @@ class InteractiveRecord
   def self.table_name
     self.to_s.downcase.plualize
   end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true 
+    # results_as_hash is A boolean that indicates whether rows in result sets should be returned as hashes or not. 
+    # By default, rows are returned as arrays
+    sql = "PRAGMA table_info('#{table_name}')"
+    # PRAGMA schema.table_info(table-name);
+    # ususally, returns one row for each column in the named table, 
+    # in this case returns an array of hashes, where each hash has the info on one column
+    table_info = DB[:conn].execute(sql)
+    # sets var table_name equal to the hash returned by executing the SQL stmnt
+    column_names = []
+
+    table_info.each do |col|
+      column_names << column["name"]
+    end
+    # iterate over the table_info hash
+    # and collect the value each hash's "name" key points at (the names of the columns)
+    column_names.compact 
+    # .compact gets rid of any nil values
+    # #column_names returns an array of strings, column names
+  end
   
 end
