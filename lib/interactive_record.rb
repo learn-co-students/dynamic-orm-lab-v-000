b20e882e5dc45312468dc14ac6b6b5f709e109fa
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 22:43:44 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,13 +2,11 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-
   def self.table_name
-    self.to_s.downcase.pluralize
+    "#{self.to_s.downcase}s"
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
 
@@ -20,22 +18,20 @@ class InteractiveRecord
     column_names.compact
   end
 
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
-
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
 
-  def table_name_for_insert
-    self.class.table_name
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  def table_name_for_insert
+    self.class.table_name
   end
 
   def values_for_insert
@@ -46,11 +42,8 @@ class InteractiveRecord
     values.join(", ")
   end
 
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def self.find_by_name(name)
@@ -58,16 +51,10 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(attribute)
-    column = attribute.keys[0].to_s
-    value = attribute.values[0]
-
-    sql = <<-SQL
-      SELECT * FROM #{table_name}
-      WHERE #{column} = ?
-      SQL
-
-    DB[:conn].execute(sql, value);
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
   end
-
 end
