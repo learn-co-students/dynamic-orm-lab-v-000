e0b88259e1bfbc0a894e4de4a9fc80892d0ac9cb
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-29 16:59:19 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,35 @@
+require 'pry'
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def initialize(options={})
+    binding.pry
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.map do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
+  end
+
+
+    self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
+
+
+
 end
