645b5d6e3b05d9342e9a703544fb11104e596f36
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-30 18:05:20 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -34,24 +34,37 @@ class InteractiveRecord
   end
 
   def values_for_insert
-  values = []
-  self.class.column_names.each do |col_name|
-    values << "'#{send(col_name)}'" unless send(col_name).nil?
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
   end
-  values.join(", ")
-end
 
-def save
-  sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
 
-  DB[:conn].execute(sql)
+    DB[:conn].execute(sql)
 
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-end
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
 
-def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = #{name}"
-  DB[:conn].execute(sql)
-end
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by_grade(grade)
+    sql = "SELECT * FROM #{self.table_name} WHERE grade = '#{grade}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(arg)
+    if arg.values[0].is_a? String
+      self.find_by_name(arg.values[0])
+    elsif arg.values[0].is_a? Integer
+      self.find_by_grade(arg.values[0])
+    end
+  end
 
 end
