b89388dfb9640a60cb7872b4a5c5c957185f185b
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-23 15:26:54 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -46,4 +46,20 @@ class InteractiveRecord
      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
    end
 
+   def self.find_by_name(name)
+     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+     DB[:conn].execute(sql)
+   end
+
+   def self.find_by(hashy)
+     hashy_property = nil
+     hashy_value = nil
+      hashy.each do |property, value|
+        hashy_property = property
+        hashy_value = value
+      end
+        sql = "SELECT * FROM #{self.table_name} WHERE #{hashy_property} = '#{hashy_value}'"
+        DB[:conn].execute(sql)
+ end
+
 end
