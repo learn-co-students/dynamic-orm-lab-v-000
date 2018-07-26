816adf1dc54bdaf6ad17237348ade6b47442664c
Nathan  Visak <nathan.visak@gmail.com>
2018-07-26 22:11:23 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,4 +26,41 @@ class InteractiveRecord
     end
   end
 
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def save #This is an instance method
+    sql = <<-SQL
+      INSERT INTO #{table_name_for_insert}
+      (#{col_names_for_insert})
+      VALUES (#{values_for_insert})
+      SQL
+
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def find_by(attribute:)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{self.column_names} = '#{attribute:}'"
+    DB[:conn].execute(sql)
+  end
+
 end
