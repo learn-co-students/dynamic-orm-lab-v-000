cb5165192949d3f90b7df739a7e12b60fb2cccb4
Micah Shute <micah.shute@gmail.com>
2018-08-01 11:13:46 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,12 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+    self.column_names.each do |col|
+        attr_accessor col.to_sym
+    end
+
+    def initialize(id: nil, name: name, grade: grade)
+        @name = name
+        @grade = grade
+    end
 end
