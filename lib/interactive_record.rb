278922a342495dbc83f2ecac488fdc900902f701
freeWillee <will.i.lee@outlook.com>
2018-08-11 11:48:17 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -34,6 +34,29 @@ class InteractiveRecord
   end
 
   def values_for_insert
-    
+    #need to format -> return an array of values for insert in SQL format
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+
+    DB[:conn].execute(sql)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(hash)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.first[0].to_s} = '#{hash.first[1]}'"
+    DB[:conn].execute(sql)
   end
 end
