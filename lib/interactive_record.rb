ac642c58e3f71404adf3f165d475d59f093953d8
Barbara Choy <barbchoy@gmail.com>
2018-09-10 18:03:59 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,32 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    "#{self.to_s.downcase}s"
+  end
+
+  def self.column_names
+    sql = "pragma table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    columns = []
+    table_info.each do |info|
+      columns << info["name"]
+    end
+    columns
+  end
+
+  def initialize(args={})
+
+    args.each do |key,value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
 end
