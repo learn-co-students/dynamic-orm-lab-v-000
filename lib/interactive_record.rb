9c36e9c84a92d49a7fe4ae464886193b4e246b60
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-14 22:54:25 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,27 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+require 'pry'
 class InteractiveRecord
+  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    hash = DB[:conn].execute("PRAGMA table_info('#{self.table_name}')")
+    columns_array = []
+    hash.each {|column| columns_array << column["name"]}
+    columns_array.compact
+  end
+  
+  self.column_names.each do |c_name|
+    attr_accessor c_name.to_sym
+  end
+  
+  def initialize(hash = {})
+    hash.each {|key, value| self.send("#{key}=", value)}
+  end
+  
   
 end
