157aa42ed80ea2b60dc92c8d0de1487c03b3ff91
Heidi Hlavinka <hchlav@gmail.com>
2018-11-01 01:36:06 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,11 +54,83 @@ class InteractiveRecord
   end
 
   def self.find_by(attr)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attr.keys.first} = #{attr.keys.first}"
-    # binding.pry
+    #convert integer to string - maybe if/else statement
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attr.keys.first.to_s} = #{attr.keys.first.to_s}"
     DB[:conn].execute(sql)
+    # binding.pry
     #{attr.keys} - Keys are column names, and values are items in row.
-    #Maybe convert .values to int before interpolating in second part after =
+    require_relative "../config/environment.rb"
+require 'active_support/inflector'
+require 'pry'
+
+class InteractiveRecord
+
+  def self.table_name
+    self.to_s.downcase.pluralize
   end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col|
+      values << "'#{send(col)}'" unless send(col).nil?
+    end
+    values.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    # binding.pry
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(attr)
+    #convert integer to string - maybe if/else statement
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attr.keys.first.to_s} = #{attr.keys.first.to_s}"
+    DB[:conn].execute(sql)
+    # binding.pry
+    #{attr.keys} - Keys are column names, and values are items in row.
+
+    #___________#
+    #Heidi Hlavinka 6 MINUTES AGO
+    #Ok. So, #{attr.keys.first.to_s} seemed to have worked. But now I'm grabbing an extra student hash.
+    #Aysan Isayo 4 MINUTES AGO
+    #got you, so we want to flatten the hash to grab the first element
+    #Aysan Isayo 4 MINUTES AGO
+    #{hash.keys.flatten[0].to_s}
+    end
+
+  end
+
 
 end
