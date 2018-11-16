ff78cba5c245853c2f82dd5171b0bd0dfedd305d
John Werenski <jtwerenski@gmail.com>
2018-11-16 18:29:42 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,80 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
   
+  def self.table_name 
+    self.to_s.downcase.pluralize
+  end 
+  
+  def self.column_names
+    DB[:conn].results_as_hash = true 
+    sql = "PRAGMA table_info('#{table_name}')"
+    column_names = []
+    
+    table_info = DB[:conn].execute(sql)
+    table_info.each do |column|
+      column_names << column["name"]
+    end 
+    column_names.compact
+  end 
+  
+  def initialize(options={})
+    options.each do |key, value|
+      self.send("#{key}=", value)
+    end 
+  end 
+  
+  def save 
+    sql =<<-SQL
+        INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+        VALUES (#{values_for_insert}) 
+    SQL
+    
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end 
+  
+  def self.find_by_name(name)
+    sql =<<-SQL 
+        SELECT * 
+        FROM #{table_name} 
+        WHERE name = ?
+    SQL
+    
+    DB[:conn].execute(sql, name)
+  end 
+  
+  def self.find_by(row={})
+    keys = []
+    values = []
+    row.each do |key, value|
+      keys << key.to_s 
+      values << value 
+    end 
+    sql =<<-SQL
+        SELECT * 
+        FROM #{table_name}
+        WHERE #{table_name}.#{keys[0]} = '#{values[0]}'
+    SQL
+    
+    DB[:conn].execute(sql)
+  end 
+  
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil? 
+    end 
+    values.join(", ")
+  end
+  
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end 
+  
+  def table_name_for_insert
+    self.class.table_name
+  end 
 end
