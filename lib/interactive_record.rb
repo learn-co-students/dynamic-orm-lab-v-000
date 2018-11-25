d6bd5717592374ef1ca4015be82173bee89727fe
Rachel Wyatt <rw126@evansville.edu>
2018-11-25 14:05:58 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -9,7 +9,7 @@ attr_accessor :id, :name, :grade
   def self.table_name
     self.to_s.downcase.pluralize
   end
-  
+
   def self.column_names
     DB[:conn].results_as_hash = true
 
@@ -21,8 +21,8 @@ attr_accessor :id, :name, :grade
       column_names << row["name"]
     end
     column_names.compact
-  end  
-  
+  end
+
   def initialize(attributes = {})
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
@@ -30,38 +30,41 @@ attr_accessor :id, :name, :grade
   def table_name_for_insert
     self.class.table_name
   end
-  
+
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end  
-  
+  end
+
 def values_for_insert
   values = []
   self.class.column_names.each do |col_name|
     values << "'#{send(col_name)}'" unless send(col_name).nil?
   end
   values.join(", ")
-end  
+end
 
 def save
   sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
- 
+
   DB[:conn].execute(sql)
- 
+
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
 end
 
 def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'" 
+  sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
   DB[:conn].execute(sql)
 end
 
-def self.find_by(variable)
-  sql = "SELECT * FROM #{self.table_name} WHERE #{self.values_for_insert} = '#{variable}'" 
+def self.find_by(options={})
+  sql = nil
+  options.each do |key, value|
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
+  end
   DB[:conn].execute(sql)
 end
 
-  
-  
-  
+
+
+
 end
