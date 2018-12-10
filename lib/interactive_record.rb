949e8f8d43e390c689ee6f353453cc94a73e2a7a
Azhar Shah <azharshahstl@yahoo.com>
2018-12-10 22:17:25 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,20 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name 
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names 
+    DB[:conn].results_as_hash = true 
+    
+    sql = "pragma_table_info('#{table_name}')"
+    
+    table_info = DB[:conn].execute(sql) 
+    column_names = [] 
+    table_info.each do |row| 
+      column_names << row["name"]
+    end 
+    column_names.compact
+  end
 end
