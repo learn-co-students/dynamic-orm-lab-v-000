504c73f9d362b3820fc5fd071b35c74f616bc376
Jonathan Dust <d24dust@hotmail.com>
2019-02-01 14:06:45 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+    self.column_names.each do |atr|
+        attr_accessor atr.to_sym
+    end
 end
