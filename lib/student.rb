a54a66c72dd08768eddb1a9b3012a8504ec791c1
Lamia Haider <lamia.haider@gmail.com>
2019-02-05 05:08:07 +0000

    Working on initialize for Student

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,25 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  attr_accessor :name, :grade
 
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = "PRAGMA table_info('#{table_name}')"
+    allcolumn = DB[:conn].execute(sql)
+    columname = []
+    allcolumn.each do |column|
+      columname << column["name"]
+    end
+    columname.compact
+  end
+
+  def initialize
+    @name = name
+    @grade = grade
+  end
 end
