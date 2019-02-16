a323bff3659ee3ced5e5b9203d6d955ebc1ad055
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-02-16 18:13:16 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,4 +58,15 @@ def self.find_by_name(name)
 end
 
 
+def self.find_by(attribute_hash)
+  #  value = attribute_hash.values.first
+  #  formatted_value = value.class == Fixnum ? value : "'#{value}'"
+  #  sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+  #  DB[:conn].execute(sql)
+ end
+
+
+
+
+
 end
