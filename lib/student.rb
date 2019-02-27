eee31a0698a4d7a0119b3d13ab78f53870b30237
Lamia Haider <lamia.haider@gmail.com>
2019-02-27 19:30:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,25 +3,11 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  attr_accessor :name, :grade
 
-  def self.table_name
-    self.to_s.downcase.pluralize
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
   end
 
-  def self.column_names
-    DB[:conn].results_as_hash = true
-    sql = "PRAGMA table_info('#{table_name}')"
-    allcolumn = DB[:conn].execute(sql)
-    columname = []
-    allcolumn.each do |column|
-      columname << column["name"]
-    end
-    columname.compact
-  end
 
-  def initialize
-    @name = name
-    @grade = grade
-  end
+
 end
