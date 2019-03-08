b9cb4340cc83c9e3901ced6e489289ff7f9cca1f
sarahpaz <sarah.paz@gmail.com>
2019-03-07 20:07:42 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,12 +7,9 @@ class InteractiveRecord
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
-
     sql = "PRAGMA table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-
     column_names = []
 
     table_info.map do |column|
