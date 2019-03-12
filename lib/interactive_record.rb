1a6d7cbf57bbd8cab9a493fb0b44284a689d7341
ktochow1 <ktochow1@binghamton.edu>
2019-03-12 16:58:57 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,55 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  attr_accessor :id
+  
+  def self.table_name 
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    DB[:conn].results_as_hash = true 
+    sql = "pragma table_info('#{table_name}')"
+    table_data = DB[:conn].execute(sql)
+    column_names = []
+    table_data.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+  
+  def initialize(info = {})
+    info.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+  
+  def table_name_for_insert 
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if do |col|
+      col == "id"
+    end.join(", ")
+  end
+  
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless 
+      send(col_name).nil?
+    end
+    values.join(", ")
+  end 
+  
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} 
+    (#{col_names_for_insert}) 
+    VALUES (#{values_for_insert})"
+    
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT * FROM #{table_name_for_insert} WHERE id = 1")[0][0]
+  end
   
 end
--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,22 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+ attr_accessor :id, :name, :grade
+ 
+ def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 
+  # def self.find_by(value)
+  #   sql = "SELECT * FROM #{self.table_name}"
+  #   DB[:conn].execute(sql)
+  # end
+  
+  def self.find_by(attr_hash)
+    value = attr_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attr_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
+  end
 end
