861add58359481d7a18982068701db09c2b8fdfa
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-13 15:01:25 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -47,4 +47,14 @@ class InteractiveRecord
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(hash)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
+    DB[:conn].execute(sql)
+  end
 end
