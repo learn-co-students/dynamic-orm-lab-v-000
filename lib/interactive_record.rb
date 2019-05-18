d202f3508b52adc83394bf2ac84d7893b49be986
Paul Fell <swellpf@gmail.com>
2019-05-18 19:46:27 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,4 +21,29 @@ class InteractiveRecord
   column_names.compact
   end
 
+  def table_name_for_insert
+
+  end
+
+  def col_names_for_insert
+
+  end
+
+  def values_for_insert
+
+  end
+
+  def save
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def self.find_by
+
+  end
+
+  
 end
