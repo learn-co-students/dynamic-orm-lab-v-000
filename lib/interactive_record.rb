1229b91868ffd7b7f8fbfe38f7b574b0833282e2
Paul Fell <swellpf@gmail.com>
2019-05-20 20:14:34 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,7 +38,7 @@ class InteractiveRecord
   end
 
   def save
-    binding.pry
+    #binding.pry
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
