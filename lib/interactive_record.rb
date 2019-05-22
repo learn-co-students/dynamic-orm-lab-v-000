06cdfc83a7457fbbddc9c94de01de00b9e6a3cff
Diapause <diapause@protonmail.com>
2019-05-22 14:27:21 -0700

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,43 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    table = DB[:conn].execute("PRAGMA table_info('#{self.table_name}')")
+    table.collect{|column| column['name']}.compact
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if{|column| column == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    columns = self.class.column_names.delete_if{|column| column == "id"}
+    columns.collect{|column|"'#{send(column)}'"}.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(hash)
+    column = hash.keys.join(', ')
+    value = hash.values.join(', ')
+    sql = "SELECT * FROM #{self.table_name} WHERE #{column} = '#{value}'"
+    DB[:conn].execute(sql)
+  end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,10 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  def initialize(attributes={})
+    attributes.each{|k,v| self.send("#{k}=", v) }
+  end
+
+  self.column_names.each{ |column| attr_accessor column.to_sym }
 
 end
