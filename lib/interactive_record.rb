ebf60874717d10337c3a44753e17917989b3ced8
Karen Flores <karen.ag.flores@gmail.com>
2019-06-21 23:45:54 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,7 +8,7 @@ class InteractiveRecord
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
+  #  DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
 
@@ -30,16 +30,16 @@ class InteractiveRecord
     self.class.table_name
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def save
